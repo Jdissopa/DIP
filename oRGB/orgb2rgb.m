d5fc4dc641386f4bfd35cd0742b658f39ff81885
jessada-dissopa <jessada.ranoni@gmail.com>
2019-03-21 21:11:19 +0700

    added 1 function: rgb2orgb

---
+++ oRGB/orgb2rgb.m
@@ -0,0 +1,13 @@
+function [rgbMtrx, rgb3Layer] = orgb2rgb(orgb, dimension)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+    %convert orgb to lcc
+    lcc = orgb2lcc(orgb);
+    
+    %convert lcc to rgb
+    rgbMtrx = lcc2rgb(lcc);
+    rgb3Layer = reshape(rgbMtrx', dimension);
+
+end
+
--- oRGB/rgb2orgb.m
+++ oRGB/rgb2orgb.m
@@ -1,40 +1,11 @@
-function image = rgb2orgb(input)
+function orgb = rgb2orgb(input)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-    %reshape the rgb into 2D matrix 3 x N
-    rgb2D = double(reshape(input, [], 3)');
+    %rgb to lcc
+    lcc = rgb2lcc(input);
     
-    %tranform matrix to transform rgb to L,C1,C2
-    tran_rgb2lcc = [0.299 0.587 0.114;
-                    0.5 0.5 -1.0;
-                    0.866 -0.866 0.0];
-    %get L,C1,C2
-    lcc = tran_rgb2lcc * rgb2D;
-    
-    %find thetas
-    c1 = lcc(2,:);
-    c2 = lcc(3,:);
-    thetas = atan2(c2, c1);
-    
-    %find thetaOs
-    thetaOs = thetas;
-    thetaOs(thetas >= 0 & thetas < pi/3) = (3/2)*thetas(thetas >= 0 & thetas < pi/3);
-    thetaOs(thetas >= pi/3 & thetas <= pi) = (pi/2)+(3/4)*(thetas(thetas >= pi/3 & thetas <= pi) - (pi/3));
-    thetaOs(thetas > -pi & thetas <= -pi/3) = -pi + (3/4)*(thetas(thetas > -pi & thetas <= -pi/3) + pi);
-    thetaOs(thetas > -pi/3 & thetas < 0) = (-pi/2) + (3/2)*(thetas(thetas > -pi/3 & thetas < 0)+(pi/3));
-    
-    %calculate theta diff
-    theta_diff = thetaOs - thetas;
-    
-    %loop for convert C1, C2 to Cyb, Crg
-    Cybrg = zeros(2, size(theta_diff, 2));
-    for I = 1:size(theta_diff, 2)
-        Cybrg(:, I) = [cos(theta_diff(I)) sin(theta_diff(I)); 
-                        -sin(theta_diff(I)) cos(theta_diff(I))] * [c1(I); c2(I)];
-    end
-    
-    lcc(2:3,:) = Cybrg;
-    image = lcc;
+    %lcc to orgb
+    orgb = lcc2orgb(lcc);
 end
 
