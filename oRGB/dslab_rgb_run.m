d1aad24adfbd15a2c160cd42917a3b8e1310814c
jessada-dissopa <jessada.ranoni@gmail.com>
2019-08-13 20:33:46 +0700

    edit lab2rgb and rgb2lab call

--- oRGB/dslab_rgb_run.m
+++ oRGB/dslab_rgb_run.m
@@ -1,14 +1,16 @@
 
-im = sat;
+%im = sat;
+im = x14_L;
 
 %lab
-lab = rgb2lab(im);
+lab = rgb2labOCV(im);
 dslab = dstretch(lab,[120 120 120]', [50 50 50]');
-rgb = lab2rgb(dslab);
-rgb = rgb2displayablergb(rgb);
+rgb = lab2rgbOCV(dslab);
+%rgb = rgb2displayablergb(rgb);
+rgb = uint8(rgb * 255);
 figure, imshow(rgb)
 
 %rgb
-dsrgb = dstretch(double(im),[120 120 120]', [50 50 50]');
-rgb = rgb2displayablergb(dsrgb);
-figure, imshow(rgb)
+%dsrgb = dstretch(double(im),[120 120 120]', [50 50 50]');
+%rgb = rgb2displayablergb(dsrgb);
+%figure, imshow(rgb)
